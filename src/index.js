8e4beba0aa2785bb90fe91836293fc2f420a03d2
kn1cht <16277200+kn1cht@users.noreply.github.com>
2019-12-31 23:52:28 +0900

    add use-si-units

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 module.exports = {
     rules: {
         'unify-kuten-and-touten': require('./unify-kuten-and-touten.js'),
+        'use-si-units': require('use-si-units')
     },
     rulesConfig: {
         'unify-kuten-and-touten': true,
+        'use-si-units': true
     }
 };
