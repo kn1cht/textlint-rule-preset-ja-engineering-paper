d579bd4b1d743a39a487447853e0e99d14a35417
kn1cht <16277200+kn1cht@users.noreply.github.com>
2020-01-04 23:49:54 +0900

    add rule: no-mix-dearu-desumasu

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 'use strict';
 module.exports = {
     rules: {
+        'no-mix-dearu-desumasu': require('textlint-rule-no-mix-dearu-desumasu'),
         'unify-kuten-and-touten': require('./unify-kuten-and-touten.js'),
-        'use-si-units': require('use-si-units')
+        'use-si-units': require('textlint-rule-use-si-units'),
     },
     rulesConfig: {
         'unify-kuten-and-touten': true,
-        'use-si-units': true
+        'use-si-units': true,
+        'no-mix-dearu-desumasu': {
+            "preferInHeader": "",
+            "preferInBody": "である",
+            "preferInList": "である",
+            "strict": true
+        },
     }
 };
