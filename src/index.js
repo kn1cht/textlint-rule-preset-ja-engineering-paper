d30a5a4e95e7401abd11df056ab5ab286bac92c7
kn1cht <16277200+kn1cht@users.noreply.github.com>
2020-01-05 12:50:08 +0900

    add external rules

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@
 'use strict';
 module.exports = {
     rules: {
-        'no-mix-dearu-desumasu': require('textlint-rule-no-mix-dearu-desumasu'),
+        'ja-hiragana-fukushi': require('textlint-rule-ja-hiragana-fukushi'),
+        'ja-hiragana-keishikimeishi': require('textlint-rule-ja-hiragana-keishikimeishi'),
+        'ja-hiragana-hojodoushi': require('textlint-rule-ja-hiragana-hojodoushi'),
+        'no-synonyms': require('@textlint-ja/textlint-rule-no-synonyms'),
+        'prh': require('textlint-rule-prh'),
         'unify-kuten-and-touten': require('./unify-kuten-and-touten.js'),
         'use-si-units': require('textlint-rule-use-si-units'),
     },
     rulesConfig: {
+        'ja-hiragana-fukushi': true,
+        'ja-hiragana-keishikimeishi': true,
+        'ja-hiragana-hojodoushi': true,
+        'no-synonyms': true,
+        'prh': {
+            'rulePaths': [
+                'node_modules/prh/prh-rules/media/WEB+DB_PRESS.yml'
+            ]
+        },
         'unify-kuten-and-touten': true,
         'use-si-units': true,
-        'no-mix-dearu-desumasu': {
-            "preferInHeader": "",
-            "preferInBody": "である",
-            "preferInList": "である",
-            "strict": true
-        },
     }
 };
